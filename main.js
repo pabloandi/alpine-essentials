80a3d2ba31c352e66d963f0abbc9e3e02fd559f0
Pablo Diaz <pabloandi@gmail.com>
2020-12-14 12:11:05 -0500

    lesson complete

--- main.js
+++ main.js
@@ -1,78 +1,19 @@
-function pause(milliseconds = 1000) {
-    return new Promise((resolve) => setTimeout(resolve, milliseconds));
-}
+function formulario() {
+    return {
+            form:  {
+                name: ''
+            },
 
-function flash(message){
-    window.dispatchEvent(new CustomEvent('flash', {
-        detail: { message }
-    }));
-}
+            user: null,
 
-function game(){
-    return { 
-        cards: [
-            { color: 'green', flipped: false, cleared: false },
-            { color: 'red', flipped: false, cleared: false },
-            { color: 'blue', flipped: false, cleared: false },
-            { color: 'yellow', flipped: false, cleared: false },
-            { color: 'green', flipped: false, cleared: false },
-            { color: 'red', flipped: false, cleared: false },
-            { color: 'blue', flipped: false, cleared: false },
-            { color: 'yellow', flipped: false, cleared: false }
-        ].sort(() => Math.random() - 0.5),
-
-        get flippedCards(){
-            return this.cards.filter(card => card.flipped);
-        },
-
-        get clearedCards(){
-            return this.cards.filter(card => card.cleared);
-        },
-
-        get remainingCards(){
-            return this.cards.filter(card => ! card.cleared);
-        },
-
-        get points(){
-            return this.clearedCards.length;
-        },
-
-        async flipCard(card){
-            if(this.flippedCards.length === 2){
-                return;
+            submit(){
+                fetch('https://reqres.in/api/users', {
+                    method: 'POST',
+                    headers: { 'Content-type': 'application/json'},
+                    body: JSON.stringify(this.form)
+                })
+                .then(response => response.json())
+                .then(user => this.user = user);
             }
-
-            card.flipped = ! card.flipped;
-
-            if (this.flippedCards.length === 2){
-
-                
-                if (this.hasMatch()){
-                    
-                    flash('You found a match!');
-
-                    await pause();
-                    this.flippedCards.forEach(card => card.cleared = true);
-
-
-                    if(! this.remainingCards.length)
-                        alert('you won');
-                    
-                        
-                }
-
-                await pause();
-                this.flippedCards.forEach(card => card.flipped = false);
-
-            }
-
-
-        },
-
-        hasMatch(){
-            return this.flippedCards[0]['color'] === this.flippedCards[1]['color']
-        },
-
-
-    }
+    };
 }
